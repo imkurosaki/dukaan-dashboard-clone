9d72ae6f98a3f2c24b3f51dd11d5a4c7bb549b10
Kean Serna <keandelaserna11@gmail.com>
2024-02-12 19:47:29 +0800

    Adding the side bar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,31 @@
+import nishyan from '/public/nishyan.svg'
+import { menuList } from '../../db/menuList'
+import Credits from './Credits'
 
 export default function Sidebar() {
-    return <div className="hidden lg:block h-screen sticky top-0 w-64">
+    return <div className="hidden lg:block h-screen sticky top-0 w-64 bg-blue-950 p-4 text-white">
+        <div className='flex justify-between'>
+            <div className='flex gap-4'>
+                <img src={`${nishyan}`} className='rounded-xl w-10' />
+                <div>
+                    <p className='text-sm'>Nishyan</p>
+                    <a href="" className='text-xs underline font-light text-gray-300'>Visit Store</a>
+                </div>
+            </div>
+            <div className='flex items-center'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="currentColor" className="w-5 h-5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </div>
+        </div>
+        <div className='flex flex-col gap-1 mt-4'>
+            {menuList.map((menu) => {
+                return <button key={menu.id} className='flex gap-3 items-center text-sm hover:bg-gray-500 px-4 py-2 rounded'>
+                    <img src={`${menu.icon}`} className='h-4 w-4'/>
+                    {menu.menu}
+                </button>
+            })}
+        </div>
+        <Credits />
     </div>
 }
---
+++ src/db/menuList.js
@@ -0,0 +1,69 @@
+import products from '/public/products.png'
+import plugins from '/public/plugins.png'
+import orders from '/public/orders.png'
+import marketing from '/public/marketing.png'
+import home from '/public/home.png'
+import discounts from '/public/discounts.png'
+import delivery from '/public/delivery.png'
+import audience from '/public/audience.png'
+import appearence from '/public/appearence.png'
+import analytics from '/public/analytics.png'
+import payouts from '/public/payouts.png'
+
+export const menuList = [
+    {
+        id: 1,
+        menu: "Home",
+        icon: home
+    },
+    {
+        id: 2,
+        menu: "Orders",
+        icon: orders
+    },
+    {
+        id: 3,
+        menu: "Products",
+        icon: products
+    },
+    {
+        id: 4,
+        menu: "Delivery",
+        icon: delivery
+    },
+    {
+        id: 5,
+        menu: "Marketing",
+        icon: marketing
+    },
+    {
+        id: 6,
+        menu: "Analytics",
+        icon: analytics
+    },
+    {
+        id: 7,
+        menu: "Payouts",
+        icon: payouts
+    },
+    {
+        id: 8,
+        menu: "Discounts",
+        icon: discounts
+    },
+    {
+        id: 9,
+        menu: "Audience",
+        icon: audience
+    },
+    {
+        id: 10,
+        menu: "Appearence",
+        icon: appearence
+    },
+    {
+        id: 11,
+        menu: "Plugins",
+        icon: plugins
+    },
+]
